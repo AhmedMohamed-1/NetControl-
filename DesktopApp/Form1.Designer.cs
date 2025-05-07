b48f81c9b869ac10c158e886e840c996e9126af4
Ahmed Mohamed <Ahmedmohamed.a5562@gmail.com>
2025-05-07 20:11:38 +0300

    Uplaoding my project

---
+++ DesktopApp/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Internet
+{
+    partial class Internet_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Internet_Form));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_WriteTheCode = new System.Windows.Forms.Label();
+            this.btn_GO = new System.Windows.Forms.Button();
+            this.label_Tittle = new System.Windows.Forms.Label();
+            this.label_TimeLeft = new System.Windows.Forms.Label();
+            this.btn_Minimize = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.pictureBox_Logo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(145, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox_Change);
+            // 
+            // label_WriteTheCode
+            // 
+            this.label_WriteTheCode.AutoSize = true;
+            this.label_WriteTheCode.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_WriteTheCode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label_WriteTheCode.Location = new System.Drawing.Point(394, 111);
+            this.label_WriteTheCode.Name = "label_WriteTheCode";
+            this.label_WriteTheCode.Size = new System.Drawing.Size(172, 21);
+            this.label_WriteTheCode.TabIndex = 1;
+            this.label_WriteTheCode.Text = "اكتب الكود الخاص بك هنا";
+            // 
+            // btn_GO
+            // 
+            this.btn_GO.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btn_GO.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_GO.Enabled = false;
+            this.btn_GO.FlatAppearance.BorderSize = 0;
+            this.btn_GO.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(184)))));
+            this.btn_GO.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(142)))), ((int)(((byte)(224)))));
+            this.btn_GO.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_GO.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_GO.ForeColor = System.Drawing.Color.White;
+            this.btn_GO.Location = new System.Drawing.Point(12, 105);
+            this.btn_GO.Name = "btn_GO";
+            this.btn_GO.Size = new System.Drawing.Size(109, 32);
+            this.btn_GO.TabIndex = 2;
+            this.btn_GO.Text = "افتح";
+            this.btn_GO.UseVisualStyleBackColor = false;
+            this.btn_GO.Click += new System.EventHandler(this.btn_GO_Click);
+            this.btn_GO.MouseEnter += new System.EventHandler(this.btn_GO_Enter);
+            this.btn_GO.MouseLeave += new System.EventHandler(this.btn_GO_Leave);
+            // 
+            // label_Tittle
+            // 
+            this.label_Tittle.AutoSize = true;
+            this.label_Tittle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Tittle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label_Tittle.Location = new System.Drawing.Point(44, 12);
+            this.label_Tittle.Name = "label_Tittle";
+            this.label_Tittle.Size = new System.Drawing.Size(64, 21);
+            this.label_Tittle.TabIndex = 4;
+            this.label_Tittle.Text = "Internet";
+            // 
+            // label_TimeLeft
+            // 
+            this.label_TimeLeft.AutoSize = true;
+            this.label_TimeLeft.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_TimeLeft.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.label_TimeLeft.Location = new System.Drawing.Point(257, 40);
+            this.label_TimeLeft.Name = "label_TimeLeft";
+            this.label_TimeLeft.Size = new System.Drawing.Size(24, 37);
+            this.label_TimeLeft.TabIndex = 5;
+            this.label_TimeLeft.Text = " ";
+            this.label_TimeLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Minimize
+            // 
+            this.btn_Minimize.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Minimize.BackgroundImage = global::Internet.Properties.Resources.minimize_ico;
+            this.btn_Minimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_Minimize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Minimize.FlatAppearance.BorderSize = 0;
+            this.btn_Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Minimize.Location = new System.Drawing.Point(515, 8);
+            this.btn_Minimize.Name = "btn_Minimize";
+            this.btn_Minimize.Size = new System.Drawing.Size(20, 20);
+            this.btn_Minimize.TabIndex = 7;
+            this.btn_Minimize.UseVisualStyleBackColor = false;
+            this.btn_Minimize.Click += new System.EventHandler(this.btn_Minimize_Click);
+            this.btn_Minimize.MouseEnter += new System.EventHandler(this.btn_Minimize_MouseEnter);
+            this.btn_Minimize.MouseLeave += new System.EventHandler(this.btn_Minimize_MouseLeave);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Close.BackgroundImage = global::Internet.Properties.Resources.Exit;
+            this.btn_Close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_Close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Close.FlatAppearance.BorderSize = 0;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Location = new System.Drawing.Point(540, 8);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(20, 20);
+            this.btn_Close.TabIndex = 6;
+            this.btn_Close.UseVisualStyleBackColor = false;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            this.btn_Close.MouseEnter += new System.EventHandler(this.btn_Close_MouseEnter);
+            this.btn_Close.MouseLeave += new System.EventHandler(this.btn_Close_MouseLeave);
+            // 
+            // pictureBox_Logo
+            // 
+            this.pictureBox_Logo.BackgroundImage = global::Internet.Properties.Resources.png_web_folder;
+            this.pictureBox_Logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox_Logo.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox_Logo.Name = "pictureBox_Logo";
+            this.pictureBox_Logo.Size = new System.Drawing.Size(26, 21);
+            this.pictureBox_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox_Logo.TabIndex = 3;
+            this.pictureBox_Logo.TabStop = false;
+            // 
+            // Internet_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(568, 149);
+            this.Controls.Add(this.btn_Minimize);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.label_TimeLeft);
+            this.Controls.Add(this.label_Tittle);
+            this.Controls.Add(this.pictureBox_Logo);
+            this.Controls.Add(this.btn_GO);
+            this.Controls.Add(this.label_WriteTheCode);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Internet_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Internet";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Internet_Form_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_WriteTheCode;
+        private System.Windows.Forms.Button btn_GO;
+        private System.Windows.Forms.PictureBox pictureBox_Logo;
+        private System.Windows.Forms.Label label_Tittle;
+        private System.Windows.Forms.Label label_TimeLeft;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_Minimize;
+    }
+}
+
---
+++ DesktopApp/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Internet
+{
+    public partial class Internet_Form : Form
+    {
+        public static EventLog _logger = new EventLog();
+        public Tuple<DateTime, ushort> codeInfo = null;
+        private CancellationTokenSource _monitorTokenSource = new CancellationTokenSource();
+        private Code CashedCode = new Code();
+        private System.Windows.Forms.Timer countdownTimer;
+        private DateTime endTime;
+        private  string EthernetName = "Ethernet 6"; // Change this to your Ethernet name if needed
+
+        public Internet_Form()
+        {
+            InitializeComponent();
+            InitializeButtonRegions();
+        }
+
+        private void InitializeButtonRegions()
+        {
+            // Initialize close button region
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddEllipse(0, 0, btn_Close.Width, btn_Close.Height);
+                btn_Close.Region = new Region(path);
+            }
+
+            // Initialize minimize button region
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddEllipse(0, 0, btn_Minimize.Width, btn_Minimize.Height);
+                btn_Minimize.Region = new Region(path);
+            }
+        }
+
+        private void btn_GO_Enter(object sender, EventArgs e)
+        {
+            if (btn_GO.Enabled) // only highlight if enabled
+                btn_GO.BackColor = Color.LightBlue; // Hover color
+        }
+        private void btn_GO_Leave(object sender, EventArgs e)
+        {
+            if (btn_GO.Enabled) // only highlight if enabled
+                btn_GO.BackColor = Color.Silver; // Hover color
+        }
+
+        private void TextBox_Change(object sender, EventArgs e)
+        {
+            if (IsThereAnyText(textBox1))
+                btn_GO.Enabled = true;
+            else
+                btn_GO.Enabled = false;
+        }
+        private bool IsThereAnyText(TextBox textBox)
+        {
+            return !string.IsNullOrWhiteSpace(textBox.Text);
+        }
+
+        private async void btn_GO_Click(object sender, EventArgs e)
+        {
+            btn_GO.Enabled = false;
+            await Enable();
+            ProcessCode(textBox1.Text.ToUpper());
+            // Re-enable after 10 seconds
+            await Task.Delay(10000);
+            btn_GO.Enabled = true;
+            btn_GO.BackColor = Color.Silver;
+        }
+        private void ProcessCode(string text)
+        {
+            try
+            {
+                var CODE = GetCode(text);
+                if (CODE != null && IsCodeValid(CODE.StartTime,CODE.Duration))
+                {
+                    CashedCode.TheCode = CODE.TheCode;
+                    CashedCode.Duration = CODE.Duration;
+                    CashedCode.StartTime = CODE.StartTime;
+                    CashedCode.Status = CODE.Status;
+
+                    MessageBox.Show("Use Your Minutes Online Wisely !" , "You Are Online :)" ,MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    StartCountdown(CashedCode.StartTime, CashedCode.Duration);
+                }
+                else
+                {
+                    Disable();
+                    MessageBox.Show("The Code is invalid or used before");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void StartCountdown(DateTime startTime, int durationInMinutes)
+        {
+            endTime = startTime.AddMinutes(durationInMinutes);
+
+            countdownTimer = new System.Windows.Forms.Timer();
+            countdownTimer.Interval = 50; // update ~20 times/sec
+            countdownTimer.Tick += CountdownTimer_Tick;
+            countdownTimer.Start();
+        }
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan remaining = endTime - DateTime.Now.AddHours(-1);
+            
+
+            if (remaining <= TimeSpan.Zero)
+            {
+                countdownTimer.Stop();
+                Disable();
+                label_TimeLeft.Text = "00:00";
+            }
+            else
+            {
+                int totalMinutes = (int)remaining.TotalMinutes;
+                int seconds = remaining.Seconds;
+                label_TimeLeft.Text = $"{totalMinutes:D2}:{seconds:D2}";
+            }
+        }
+
+        private void Disable()
+        {
+            try
+            {
+                Console.WriteLine("Attempting to disable Ethernet...");
+                using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionID IS NOT NULL"))
+                {
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        string name = obj["NetConnectionID"]?.ToString();
+
+                        if (name != null && name.StartsWith("Ethernet") && name == EthernetName)
+                        {
+                            var result = obj.InvokeMethod("Disable", null);
+                            Console.WriteLine($"Successfully disabled {name}: {result}");
+                            return;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error disabling Ethernet: {ex.Message}");
+            }
+        }
+        private async Task Enable()
+        {
+            try
+            {
+                Console.WriteLine("Attempting to enable Ethernet...");
+                using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionID IS NOT NULL"))
+                {
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        string name = obj["NetConnectionID"]?.ToString();
+
+                        if (name != null && name.StartsWith("Ethernet") && name == EthernetName)
+                        {
+                            // Perform the enabling operation asynchronously.
+                            var result = await Task.Run(() => obj.InvokeMethod("Enable", null));
+                            Console.WriteLine($"Successfully enabled {name}: {result}");
+                            return;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error enabling Ethernet: {ex.Message}");
+            }
+        }
+
+
+        public bool IsCodeValid(DateTime startTime, int durationMinutes)
+        {
+            return TimeLeft(startTime, durationMinutes);
+        }
+        public bool TimeLeft(DateTime startTime, int duration)
+        {
+            TimeSpan timeLeft = (startTime.AddMinutes(duration)) - DateTime.Now.AddHours(-1);
+
+            if (timeLeft.TotalSeconds > 0)
+            {
+                Console.WriteLine($"⏳ Code valid for {Math.Ceiling(timeLeft.TotalMinutes)} more minutes.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("⛔ Code expired.");
+                return false;
+            }
+        }
+
+
+        public Code GetCode(string code)
+        {
+            string connectionString = "Server=db18170.public.databaseasp.net; Database=db18170; User Id=db18170; Password=D!n9mA8@_sL7; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True;";
+
+            string query = "SELECT TOP 1 * FROM Codes WHERE TheCode = @code AND Status = 1";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@code", code);
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Code
+                        {
+                            Id = (int)reader["Id"],
+                            TheCode = (string)reader["TheCode"],
+                            StartTime = (DateTime)reader["StartTime"],
+                            Duration = (int)reader["Duration"],
+                            Status = (int)reader["Status"]
+                        };
+                    }
+                }
+            }
+
+            return null; // Not found or inactive
+        }
+
+        public class Code
+        {
+            public int Id { get; set; }
+
+            public string TheCode { get; set; }
+
+            public DateTime StartTime { get; set; }
+            public int Duration { get; set; }
+
+            public int Status { get; set; }
+        }
+
+
+        private void btn_Close_MouseEnter(object sender, EventArgs e)
+        {           
+            btn_Close.BackColor = Color.FromArgb(232, 17, 35); // Red color for close button
+        }
+        private void btn_Close_MouseLeave(object sender, EventArgs e)
+        {           
+            btn_Close.BackColor = Color.Transparent;
+        }
+        private void btn_Minimize_MouseEnter(object sender, EventArgs e)
+        {
+            btn_Minimize.BackColor = Color.FromArgb(232, 17, 35); // Red color for minimize button
+        }
+        private void btn_Minimize_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Minimize.BackColor = Color.Transparent;
+        }
+
+        private void btn_Minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            Disable();
+            Application.Exit();
+        }
+
+        private void Internet_Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disable();
+        }
+    }
+}
